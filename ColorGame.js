c4dd2dad97f52618188be0842133ce8a8e0ae3a3
Abhay <abhayk97@gmail.com>
2018-08-18 17:23:00 +0530

    fully functional game without features and visuals end-p4

--- ColorGame.js
+++ ColorGame.js
@@ -1,17 +1,9 @@
-var colors = [" rgb(255, 0, 0)",
-				"rgb(255, 255, 0)",
-				"rgb(0, 255, 0)",
-				"rgb(0, 255, 255)",
-				"rgb(0, 0, 255)",
-				"rgb(255, 0, 255)",
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-
 var pickedColor = pickColor();
-
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,12 +13,12 @@ for(var i=0;i<squares.length;i++){
 	//add click listeners to squares
 	squares[i].addEventListener("click",function(){
 		//grab color of clicked square
-		var clickedColor = this.style.backgroundColor;
+		var clickedColor=this.style.backgroundColor;
 		//compare color to picked color
 		if(clickedColor===pickedColor){
 			messageDisplay.textContent = "correct";
 			changeColors(clickedColor);
-
+			h1.style.backgroundColor = clickedColor;
 		}
 		else{
 			this.style.backgroundColor = "#232323";
@@ -34,23 +26,31 @@ for(var i=0;i<squares.length;i++){
 		}
 	});
 }
-
-
 function changeColors(color){
-	//loop thru all squares 
 	for(var i = 0;i<squares.length; i++){
 		squares[i].style.backgroundColor = color;
 	}
-	//change each color to match given color
 }
-
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
 }
-
-
-
+function generateRandomColors(num){
+	var arr = [];
+	for(var i = 0; i < num; i++){
+		arr.push(randomColor());
+	}
+	return arr;
+}
+function randomColor(){
+	//pick a red from 0 to 255
+	var r = Math.floor(Math.random()*256);
+	//pick a green from 0 to 255
+	var g =Math.floor(Math.random()*256);
+	//pick a blue from 0 to 255
+	var b =Math.floor(Math.random()*256);
+	return "rgb(" +r + "," + " "+g + "," + " "+b + ")";
+}
 
 
 
